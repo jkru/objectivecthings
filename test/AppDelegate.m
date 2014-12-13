e39af309e4e328a2f2409c54662678c42ca908a4
Glenna Buford <glenna@apportable.com>
2014-12-13 14:49:23 -0800

    initial commit

--- test/AppDelegate.m
+++ test/AppDelegate.m
@@ -8,16 +8,77 @@
 
 #import "AppDelegate.h"
 
-@interface AppDelegate ()
+@interface Movie : NSObject
+
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *posterURL;
+@property (nonatomic, copy) NSString *synopsis;
+
 
 @end
 
-@implementation AppDelegate
+@implementation Movie
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
+    Movie *movie = [[Movie alloc] init];
+    movie.title = @"Best movie ever";
+    movie.posterURL = @"http://best.com";
+
+    Movie *movie2 = [[Movie alloc] init];
+    movie2.title = @"scary movie ever";
+    movie2.posterURL = @"http://scary.com";
+    
+    NSArray *movies = @[movie, movie2];
+    for (Movie *m in movies){
+        NSLog(@"Movie Title: %@", m.title);
+    }
+    
+    BOOL isTrue = YES; // can only be YES or NO, can't call a method on it
+    CGFloat x = 10.0;  // CG is core graphics, it's a float
+    double d = 10.0;   // more scalar types
+    float f = 10.0;
+    
+    NSNumber *number = [NSNumber numberWithBool: isTrue]; //turn a scalar type into an object
+    number = @10.0;    // put an @ sign in front of a thing, it turns it into an object AKA literal notation
+    
+    NSDictionary *dict =@{
+                          @"isUser": @YES,
+    };
+    
+    NSArray *array =@[
+        @5,@6,@7
+    ];
+    
+    
+    NSDictionary *dict2 =@{
+                          @"List": @[
+                              @1,@2,@3,]
+                          };
+    
+    
+    CGFloat x2 = [array[0] floatValue];
+    
+    for (NSInteger i = 0; i<array.count; i++){
+        NSLog(@"%@", array[i]);
+    }
+    
+    for (NSNumber *num in array){
+        NSLog(@"%@",num);
+    }
+    
+    CGFloat tip = [self tipForTotal:100];
+    NSLog(@"Float: %f", tip);
+    CGFloat total = [self totalForBase:100 andTipPercentage:0.2];
+    
     return YES;
+}
+
+-(double)tipForTotal:(CGFloat)total {
+    return .2*total;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
